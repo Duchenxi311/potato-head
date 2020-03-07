c9216a9595a78ebd5a466e34cf86d0ab6b6edbbf
Duchenxi311 <1037966576@qq.com>
2020-03-07 14:56:43 +0800

    potato-head

--- app/src/main/java/mg/studio/android/lab001/MainActivity.java
+++ app/src/main/java/mg/studio/android/lab001/MainActivity.java
@@ -3,6 +3,7 @@ package mg.studio.android.lab001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +25,45 @@ public class MainActivity extends AppCompatActivity {
         mustache = findViewById(R.id.mustache);
         nose = findViewById(R.id.nose);
         shoes = findViewById(R.id.shoes);
+    }
+
+    public void check(View view, ImageView img){
+        if(view.VISIBLE==img.getVisibility()){
+            img.setVisibility(View.GONE);
+        }else{
+            img.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void cbMouth(View view){
+        check(view, mouth);
+    }
+    public void cbArms(View view){
+        check(view, arms);
+    }
+    public void cbEars(View view){
+        check(view, ears);
+    }
+    public void cbEyebrows(View view){
+        check(view, eyebrows);
+    }
+    public void cbEyes(View view){
+        check(view, eyes);
+    }
+    public void cbGlasses(View view){
+        check(view, glasses);
+    }
+    public void cbHat(View view){
+        check(view, hat);
+    }
+    public void cbMustache(View view){
+        check(view, mustache);
+    }
+    public void cbNose(View view){
+        check(view, nose);
+    }
+    public void cbShoes(View view){
+        check(view, shoes);
     }
 
 }
